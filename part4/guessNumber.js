a73a385c7202d39dc5bbefa8c9c00dadbb6aa010
pichsineemook <pichsinee6.codercamp@gmail.com>
2022-02-02 16:26:43 +0700

    update5

--- part4/guessNumber.js
+++ part4/guessNumber.js
@@ -5,9 +5,11 @@ let max = 100;
 let number = prompt('Guess the number between ' + min + ' and ' + max + ' inclusive or input other numbers to see the secret number?');
 while( number != randomNum )
 {
-    if (randomNum <= number)
+    if (randomNum <= number){
         max = number ;
-    else min = number ;
+    }
+    else{ min = number ;}
+
     number = prompt('Guess the number between ' + min + ' and ' + max + ' inclusive or input other numbers to see the secret number?');
 }
 let win = alert('Matched! You win.\nThe secret number is ' + randomNum );
